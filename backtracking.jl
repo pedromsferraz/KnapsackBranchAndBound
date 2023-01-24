c2aad4e264e5bacdb8bc0ba67d6cff04e47061c1
Pedro Maia de Sampaio Ferraz <pedroferraz12@gmail.com>
2023-01-23 20:02:35 -0500

    Update backtracking.jl

--- backtracking.jl
+++ backtracking.jl
@@ -1,11 +1,30 @@
-function backtracking_knapsack(n, B, v, w)
+# Implementação que percorre todas as 2^n combinações de itens e retorna a de maior valor
+function backtracking_knapsack_complete(n, B, v, w)
+    function backtracking_knapsack_inner(i, value, weight)
+        if i == 0
+            if weight <= B
+                return value
+            else
+                return 0
+            end
+        end
+
+        ans1 = backtracking_knapsack_inner(i-1, value, weight)
+        ans2 = backtracking_knapsack_inner(i-1, value + v[i], weight + w[i])
+        return max(ans1, ans2)
+    end
+    return backtracking_knapsack_inner(n, 0, 0)
+end
+
+# Implementação que percorre apenas as combinações viáveis
+function backtracking_knapsack_feasible(n, B, v, w)
     function backtracking_knapsack_inner(i, q, value, weight)
         if i == 0
             return value
         end
 
         ans1 = backtracking_knapsack_inner(i-1, q, value, weight)
-        ans2 = -Inf64
+        ans2 = 0
         if weight + w[i] <= q
             ans2 = backtracking_knapsack_inner(i-1, q, value + v[i], weight + w[i])
         end
@@ -13,6 +32,3 @@ function backtracking_knapsack(n, B, v, w)
     end
     return backtracking_knapsack_inner(n, B, 0, 0)
 end
-
-# instance = generate_knapsack_inst(28)
-# backtracking_knapsack(instance...)
